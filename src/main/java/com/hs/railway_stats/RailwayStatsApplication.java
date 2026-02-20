cc851d905425469154cc6430f1f236999ec2efb6
hasshe <hassan.sheikha_97@outlook.com>
2026-02-21 00:25:55 +0100

    initial commit

---
+++ src/main/java/com/hs/railway_stats/RailwayStatsApplication.java
@@ -0,0 +1,13 @@
+package com.hs.railway_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayStatsApplication.class, args);
+	}
+
+}
