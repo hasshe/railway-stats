21889dc7bd509c8f659de7a3acb5bc018f539b45
hasshe <hassan.sheikha_97@outlook.com>
2026-02-22 15:53:11 +0100

    set activeprofiles on tests

--- src/test/java/com/hs/railway_stats/RailwayStatsApplicationTests.java
+++ src/test/java/com/hs/railway_stats/RailwayStatsApplicationTests.java
@@ -2,7 +2,9 @@ package com.hs.railway_stats;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles({"test", "dev"})
 @SpringBootTest
 class RailwayStatsApplicationTests {
 
